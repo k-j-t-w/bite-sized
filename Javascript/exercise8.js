632bdd951c7e3e5990e0475a0ba13c82837aeb59
k-j-t-w <kaiwingfield17@gmail.com>
2025-02-09 21:53:49 -0800

    build structure for exercise 8

---
+++ Javascript/exercise8.js
@@ -0,0 +1,21 @@
+// 1. Implement a debounce function in JavaScript that limits the frequency of a function’s execution when it’s called repeatedly within a specified time frame. 
+
+// 2. Write a function that takes an array of objects and a key, and returns a new array sorted based on the values of that key in ascending order. 
+
+// 5. Implement a function that takes two sorted arrays and merges them into a single sorted array without using any built-in sorting functions. 
+
+// 8. Implement a function that flattens a nested array in JavaScript, converting it into a single-level array. 
+
+// 9. Write a function that determines if two strings are anagrams of each other  
+
+// 2. Implement a function to reverse a string without using the built-in reverse() method. 
+
+// 3. Given an array of numbers, write a function to find the largest and smallest numbers in the array. 
+
+// 4. Write a function that takes an array of integers as input and returns a new array with only the unique elements. 
+
+// 7. Implement a function to find the sum of all the numbers in an array. 
+
+// 9. Implement a function to remove duplicates from an array. 
+
+// 10. Write a function that sorts an array of numbers in ascending order. 
